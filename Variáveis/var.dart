10882055d5c9d882828580d2c0884443972fd3ec
Hacerfak <hacerfak@outlook.com>
2022-02-10 18:06:09 -0300

    Declaração de variáveis

---
+++ Variáveis/var.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  //Variável sem tipagem.
+  var name = 'Eder 1';
+  print(name);
+
+  //Variável com tipagem declarada.
+  String sName = 'Eder 2';
+  print(sName);
+
+  //Variável do tipo Object ou dynamic.
+  Object oName = 'Eder 321';
+  print(oName);
+
+  dynamic dName = 'Eder 123';
+  print(dName);
+}
