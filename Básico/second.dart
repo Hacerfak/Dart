a024a2a08490ee5ae18e8b5501f183e633de6ad8
Hacerfak <hacerfak@outlook.com>
2022-02-11 14:18:50 -0300

    validação de argumentos

--- Básico/second.dart
+++ Básico/second.dart
@@ -1,8 +1,20 @@
-void printArgs(Object numberToPrint) {
-  print('O número é $numberToPrint.');
+void printArgs(int numberToPrint, int position) {
+  print('O número do argumento $position é $numberToPrint.');
 }
 
 void main(List<String> args) {
-  //Preencher o argumento de execução com um inteiro ou float.
-  printArgs(args[0]);
+  //Preencher o argumento de execução com um inteiro.
+  if (args.isEmpty) {
+    print(
+        'Preencha ao menos um valor númerico valido como argumento. Ex: dart second.dart 5 10 20');
+  }
+
+  for (var i = 0; i < args.length; i++) {
+    var value = int.tryParse(args[i]);
+    if (value == null) {
+      print('Nenhum valor númerico válido para o argumento $i.');
+    } else {
+      printArgs(int.parse(args[i]), i);
+    }
+  }
 }
